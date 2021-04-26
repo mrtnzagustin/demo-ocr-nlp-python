5a7208651b8d27e38c3140f8874745537f0eda15
Agustin Martinez <mrtnz.agustin@gmail.com>
2021-04-26 01:09:59 +0000

    Added and endpoint to send multipart with pdf file from outside server.

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import tika
 tika.TikaClientOnly = True # Mark tika python as client online (no localhost server will be mounted)
 from tika import parser
 import os
+import uuid
+import json
 
 print("Loading Models...")
 MODELS = {
@@ -20,7 +22,7 @@ def get_model_desc(nlp, model_name):
     model_version = nlp.meta["version"]
     return f"{lang_name} - {model_name} (v{model_version})"
 
-
+"""Get models included in api"""
 @hug.get("/models")
 def models():
     return {
@@ -55,7 +57,7 @@ def ner(text: str):
         }
 
 
-@hug.get("/ner/file")
+@hug.get("/ner/file-path")
 def ner(filePath: str):
     """Get entities for file specified"""
     
@@ -75,6 +77,49 @@ def ner(filePath: str):
         return {
                 "error": False,
                 "data": {
+                    "nounPhrases": [chunk.text for chunk in doc.noun_chunks],
+                    "verbs": [token.lemma_ for token in doc if token.pos_ == "VERB"],
+                    "entitites": [ { "label": entity.label_, "text": entity.text } for entity in doc.ents ]
+                }
+            }
+    except Exception as exception:
+        return {
+            "error": True,
+            "message": exception.__str__()
+        }
+
+@hug.get("/ner/file")
+def ner(file, otherData: str):
+    """Get entities for file specified"""
+    try:
+        # Converts other data to json
+        otherData = json.loads(otherData)
+        
+        # Generates a unique uuid and save file to tmp
+        uuidString = uuid.uuid4()
+        filePath = f'.tmp/{uuidString}.pdf'
+        with open(filePath, "wb") as f:
+            f.write(file)
+
+        # Obtain the spanish model
+        nlp = MODELS["es_core_news_sm"]
+        
+        # Parse of file with apache tika (metadata and content)
+        parsed = parser.from_file(filePath, f'{os.getenv("APACHE_TIKA_SERVER")}/tika')
+
+        # remove tmp file
+        os.remove(filePath)
+        
+        # Get the metadata of the pdf file
+        # metadata = parsed['metadata']
+        
+        # Executes nlp over content
+        doc = nlp(parsed['content'])
+        
+        return {
+                "error": False,
+                "data": {
+                    "otherData": otherData,
                     "nounPhrases": [chunk.text for chunk in doc.noun_chunks],
                     "verbs": [token.lemma_ for token in doc if token.pos_ == "VERB"],
                     "entitites": [ { "label": entity.label_, "text": entity.text } for entity in doc.ents ]
